4085d9a1a2c1ae25e0f4319061b8c70959202423
Alejandro <alejandromlgranadilla@gmail.com>
2020-04-09 13:32:45 +0100

    table

--- media/js/api-solitude.js
+++ media/js/api-solitude.js
@@ -8,6 +8,35 @@ sol_button.onclick = function solicitudJSON() {
   $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId=1"  
   , success: function(result){
     console.log(result);
-    $("#contenedorSolicitud").html(result[0].postId);
+    parsearObjeto(result);
   }});
+}
+
+function parsearObjeto(result) {
+  $("#contenedorSolicitud").html(" ");
+  
+  $("#contenedorSolicitud").append("<thead><tr>");
+
+  
+    let fields = Object.keys(result[0]);
+
+    for (let i = 0; i < fields.length; i++) {
+     $("#contenedorSolicitud").append("<th>" + fields[i] + "</th>");
+    }
+  
+  $("#contenedorSolicitud").append("</tr></thead>");
+  $("#contenedorSolicitud").append("<tbody>");
+  for (let i = 0; i < result.length; i++) {
+    $("#contenedorSolicitud").append("<tr>");
+
+    $("#contenedorSolicitud").append("<td>" + result[i].postId + "</td>");
+    $("#contenedorSolicitud").append("<td>" + result[i].id + "</td>");
+    $("#contenedorSolicitud").append("<td>" + result[i].name + "</td>");
+    $("#contenedorSolicitud").append("<td>" + result[i].email + "</td>");
+    $("#contenedorSolicitud").append("<td>" + result[i].body + "</td>");
+    
+    
+    $("#contenedorSolicitud").append("</tr>");
+  }
+  $("#contenedorSolicitud").append("</tbody>");
 }
