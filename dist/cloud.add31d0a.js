8461e5a2f2aa2f35c8f27e3979b2c073c625bd66
Alejandro <alejandromlgranadilla@gmail.com>
2020-04-11 16:31:05 +0100

    basura

--- dist/cloud.add31d0a.js
+++ dist/cloud.add31d0a.js
@@ -275,15 +275,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40437" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38911" + '/');
->>>>>>> e92c9efa0df06ba29541c8016face729c844aca0
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40437" + '/');
->>>>>>> develop
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37733" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
