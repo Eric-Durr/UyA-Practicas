1772008ce5ce3345817c8d08c566b289ef282290
Alejandro <alejandromlgranadilla@gmail.com>
2020-05-25 23:06:28 +0100

    responsive page done

--- public/media/js/index.js
+++ public/media/js/index.js
@@ -4,7 +4,12 @@ console.log("hello world")
 // funciones javascript de Materialize.
  
 document.addEventListener('DOMContentLoaded', function() {
+
     M.AutoInit(); 
+      M.Carousel.init({
+      fullWidth: true,
+      indicators: true
+    });
   });
 
 
