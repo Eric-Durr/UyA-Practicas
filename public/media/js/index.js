fa38182b9ea99b65b1db42c97db199cbd58a770f
Eric DS <eric.d.s.info@gmail.com>
2020-06-06 00:54:14 +0100

    handsome landing

--- public/media/js/index.js
+++ public/media/js/index.js
@@ -6,10 +6,53 @@ console.log("hello world")
 document.addEventListener('DOMContentLoaded', function() {
 
     M.AutoInit(); 
-      M.Carousel.init({
+    M.Carousel.init({
       fullWidth: true,
       indicators: true
     });
+
+});
+
+//Slider inir
+
+$(document).ready(function(){
+  $('.slider').slider({
+    fullWidth: true,
+    indicators: false,
+    height: 900
   });
+});
 
 
+// WAI-ARIA index management
+
+  function close()
+  {
+    const side = M.Sidenav.getInstance(elem);
+  }
+
+  function changeValues(e)  
+  {
+    if(e.keyCode==13)
+    {
+      list = document.getElementById("traday-nav").getElementsByTagName("LI");
+      console.log(list);
+      for (let i = 0; i < list.length; i++)
+      {
+        list[i].childNodes[0].tabIndex=0;
+      }
+    }
+  }
+
+  function nullValues(e)  
+  {
+    if(e.keyCode==13)
+    {
+      list = document.getElementById("traday-nav").getElementsByTagName("LI");
+      console.log(list);
+      for (let i = 0; i < list.length; i++)
+      {
+        list[i].childNodes[0].tabIndex=-1;
+      }
+    }
+  }
