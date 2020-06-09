bdda8f7aedab0b9ed47f4d354172ff9e62c3648f
Oscar <oscarcigala@hotmail.com>
2020-06-09 13:51:42 +0100

    Intento de firebase

--- public/media/js/tareas.js
+++ public/media/js/tareas.js
@@ -1,8 +1,21 @@
 
-function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('lista-tareas-propuestas/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
+const boton = document.querySelector("#myButton");
+boton.addEventListener("click", () => {
+  const titulo = document.querySelector("#taskTitle").value;
+  const descripcion = document.querySelector("#taskDescription").value;
+
+  database.collection("tareas_propuestas").add({
+    Descripcion: descripcion,
+    Titulo: titulo
+  })
+
+  .then((docRef) => {
+    console.log("Tarea añadida a la bbdd por el id: ", docRef.id);
+    document.querySelector('#taskTitle').value = '';
+    document.querySelector('#taskDescription').value = '';
+  })
+
+  .catch((docRef) => {
+    console.log("Error al añadir la tarea.", Error);
+  })
+});
